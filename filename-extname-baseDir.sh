3d865b7b01b3da477223093f156eb7760e19bcac
guohh <guoguowww818@126.com>
2011-11-01 09:20:31 +0000

    more nice ideals .

---
+++ filename-extname-baseDir.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+FULLPATH=/usr/share/X11/xorg.conf.d/50-synaptics.conf
+# remove all the prefix until "/" character
+FILENAME=${FULLPATH##*/}
+# remove all the prefix unitl "." character
+FILEEXTENSION=${FILENAME##*.}
+# remove a suffix, in our case, the filename, this will return the name of the directory that contains this file
+BASEDIRECTORY=${FULLPATH%$FILENAME}
+
+echo "path = $FULLPATH"
+echo "file name = $FILENAME"
+echo "file extension = $FILEEXTENSION"
+echo "base directory = $BASEDIRECTORY"
+
